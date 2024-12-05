aa2d0e6ff0e443736df9775cffe5084e3376fd93
devcsuzarte@gmail.com <claudio.ssjr19@gmail.com>
2024-12-05 19:58:08 -0300

    Add search bar style

--- lib/constants.dart
+++ lib/constants.dart
@@ -57,4 +57,31 @@ const kInputDecoration = InputDecoration(
   ),
 );
 
+const kSearchInputDecoration = InputDecoration(
+  filled: true,
+  fillColor: Colors.white,
+  hintText: 'Pesquisar produto',
+  floatingLabelBehavior: FloatingLabelBehavior.never,
+  hintStyle: TextStyle(
+    color: Colors.grey,
+  ),
+  prefixIcon: Icon(CupertinoIcons.search),
+  contentPadding:
+  EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
+  enabledBorder: OutlineInputBorder(
+    borderSide:
+    BorderSide(color: kPrimaryColor, width: 1.0),
+    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
+  focusedBorder: OutlineInputBorder(
+    borderSide:
+    BorderSide(color: kSecondaryColor,width: 2.0),
+    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
+);
+
+
 const kTextFieldSeparator = SizedBox(height: 20);
--- lib/screens/items_screen/items_screen.dart
+++ lib/screens/items_screen/items_screen.dart
@@ -10,29 +10,48 @@ class ItemsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('myCommerce'),
-        backgroundColor: kSecondaryColor,
-        foregroundColor: kPrimaryColor,
-      ),
-          body: SafeArea(
-              child: Scaffold(
-                floatingActionButtonLocation: FloatingActionButtonLocation.miniEndFloat,
-                floatingActionButton: FloatingActionButton(
-                  onPressed: () {
-                    Navigator.push(context, CupertinoPageRoute(builder: (context) => SaleScreen())
-                    );
-                  },
-                  child: Icon(
-                    CupertinoIcons.money_dollar,
-                    size: 32,
-                    color: kSecondaryColor,),
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'myCommerce',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          backgroundColor: kSecondaryColor,
+          foregroundColor: kPrimaryColor,
+          bottom: PreferredSize(
+            preferredSize: Size(
+              MediaQuery.of(context).size.width,
+              50),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 8.0, right:  8.0, bottom: 10),
+                child: TextField(
+                  decoration: kSearchInputDecoration,
                 ),
-                body: ItemsList(),
+              ),
+          ),
+        ),
+            body: SafeArea(
+                child: Scaffold(
+                  floatingActionButtonLocation: FloatingActionButtonLocation.miniEndFloat,
+                  floatingActionButton: FloatingActionButton(
+                    onPressed: () {
+                      Navigator.push(context, CupertinoPageRoute(builder: (context) => SaleScreen())
+                      );
+                    },
+                    child: Icon(
+                      CupertinoIcons.money_dollar,
+                      size: 32,
+                      color: kSecondaryColor,),
+                  ),
+                  body: ItemsList(),
+                ),
               ),
             ),
-          );
+    );
   }
 }
 
